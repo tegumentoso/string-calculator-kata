4de60edd09bad12a19a8c3c05f6f0ca7f31d8256
Marco Dianti <marcodianti@gmail.com>
2017-06-20 17:35:47 +0200

    checking negative number exception message

--- src/main/java/marcodianti/calculator/StringCalculator.java
+++ src/main/java/marcodianti/calculator/StringCalculator.java
@@ -1,11 +1,16 @@
 package main.java.marcodianti.calculator;
 
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+import main.java.marcodianti.calculator.exceptions.NegativeNumberException;
+
 public class StringCalculator {
-	public int add(String numbers) {
+	public static String NEGATIVE_NUMBERS_EXCEPTION_TEXT="Negatives not allowed: ";
+	public int add(String numbers) throws NegativeNumberException {
 		int total = 0;
 		String delimiter = ",";
+		ArrayList<Integer> negatives=new ArrayList();
 		if (numbers.equals("")) {
 
 		} else {
@@ -19,8 +24,23 @@ public class StringCalculator {
 				String subString = st1.nextToken();
 				StringTokenizer st2 = new StringTokenizer(subString, "\n", false);
 				while (st2.hasMoreTokens()) {
-					total += Integer.parseInt(st2.nextToken());
+					int parsedInt=Integer.parseInt(st2.nextToken());
+					if(parsedInt>=0){
+						total += parsedInt;	
+					}else{
+						negatives.add(parsedInt);
+						
+					}
+					
 				}
+			}
+			//check if there were negatives
+			if(negatives.size()>0){
+				String listNegatives="";
+				for(int i=0;i<negatives.size();i++){
+					listNegatives+=negatives.get(i);
+				}
+				throw new NegativeNumberException(NEGATIVE_NUMBERS_EXCEPTION_TEXT+listNegatives);
 			}
 		}
 		return total;
--- src/test/java/marcodianti/calculator/StringCalculatorTest.java
+++ src/test/java/marcodianti/calculator/StringCalculatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 import main.java.marcodianti.calculator.StringCalculator;
+import main.java.marcodianti.calculator.exceptions.NegativeNumberException;
 
 public class StringCalculatorTest {
 	StringCalculator sc=new StringCalculator();
@@ -16,27 +17,60 @@ public class StringCalculatorTest {
 	}
 	@Test
 	public final void emptyStringGivesZero(){
-		assertEquals(0, sc.add(""));
+		try {
+			assertEquals(0, sc.add(""));
+		} catch (NegativeNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public final void sumIsCorrect(){
 		
-		assertEquals(5, sc.add("3,2"));
+		try {
+			assertEquals(5, sc.add("3,2"));
+		} catch (NegativeNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public final void sumThreeNumbers(){
 		
-		assertEquals(6, sc.add("3,2,1"));
+		try {
+			assertEquals(6, sc.add("3,2,1"));
+		} catch (NegativeNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public final void inputWithCommasAndNewlines(){
 		
-		assertEquals(6, sc.add("1\n2,3"));
+		try {
+			assertEquals(6, sc.add("1\n2,3"));
+		} catch (NegativeNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public final void inputWithCustomDelimiter(){
 		
-		assertEquals(3, sc.add("//;\n1;2"));
+		try {
+			assertEquals(3, sc.add("//;\n1;2"));
+		} catch (NegativeNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	@Test(expected = NegativeNumberException.class)
+	public void receiveNegativeNumberException()
+	        throws NegativeNumberException {
+	    sc.add("-1,-2");
+	}
+	@Test
+	public void listOfNegativesInException(){
+	       try {
+			sc.add("1,-2,3,-4");
+		} catch (NegativeNumberException e) {
+			assertEquals(StringCalculator.NEGATIVE_NUMBERS_EXCEPTION_TEXT+"-2-4",e.getMessage());
+		}
+	}
 }
